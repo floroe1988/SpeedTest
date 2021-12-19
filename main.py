7fc131b9727a3eeee55b109e7005617f39f55d7a
floroe1988 <68111417+floroe1988@users.noreply.github.com>
2021-12-19 11:31:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from src.GetConnectionSpeed import getConnectionSpeed
 from src.StoreConnectionSpeed import storeConnectionSpeed
 
 def main():
-	getConnectionSpeed()
-	storeConnectionSpeed()
-	print("all done")
+    getConnectionSpeed()
+    storeConnectionSpeed()
+    print("all done")
 
 if __name__ == "__main__":
     main()
