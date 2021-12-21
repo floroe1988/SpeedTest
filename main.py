8b273a45292eefcd1c3953f3c6b267287e7d6c4d
floroe1988 <68111417+floroe1988@users.noreply.github.com>
2021-12-21 15:57:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from src.StoreConnectionSpeed import storeConnectionSpeed
 def main():
     '''
     Function to test the current internet down- and upload speed
-    After speedtest has been performed, the result is sent to a spread sheet via an ifttt webhook
+    After speedtest has been performed, the result is sent to a spread sheet
+    via an ifttt webhook
     '''
     
     result = getConnectionSpeed()
