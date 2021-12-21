38966ec100fed041102f60f0b8b1e1ab05a1e244
floroe1988 <flo.roettenbacher@gmail.com>
2021-12-21 12:50:02 +0100

    first version of working web hook integration

--- src/StoreConnectionSpeed.py
+++ src/StoreConnectionSpeed.py
@@ -1,19 +1,23 @@
+import json
 import requests
 
 def storeConnectionSpeed():
-    url = 'https://maker.ifttt.com/trigger/{event_name}/with/key/{key}'
+    url = 'https://maker.ifttt.com/trigger/{event_name}/json/with/key/{key}'
 
     value1 = 1
     value2 = 2
     value3 = 3
+    value4 = 4
 
     body = {}
     if value1 is not None:
-        body['value1'] = value1
+        body['date'] = value1
     if value2 is not None:
-        body['value2'] = value2
+        body['time'] = value2
     if value3 is not None:
-        body['value3'] = value3
+        body['download'] = value3
+    if value4 is not None:
+        body['upload'] = value4
 
     response = requests.post(url, json=body)
     
