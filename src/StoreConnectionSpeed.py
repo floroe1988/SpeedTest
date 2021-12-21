382817d05e34a44e25fb1bf701dadcd6310fe424
floroe1988 <68111417+floroe1988@users.noreply.github.com>
2021-12-21 15:55:36 +0100

    Update StoreConnectionSpeed.py

--- src/StoreConnectionSpeed.py
+++ src/StoreConnectionSpeed.py
@@ -1,6 +1,20 @@
 import requests
 
 def storeConnectionSpeed(payload):
+    '''
+    Funtcion triggers the ifttt event specified in the tag {event_name}. This event
+    has to be specified in the url variable together with the {key} provided by ifttt
+    for the respective account.
+    
+    For this function to work, the event must be created in the user's ifttt account.
+    
+    Parameters
+    ----------
+        payload: dict
+            The payload has to contain the following entrie {date (str)}, {time (str)},
+            {download (int)} and {upload (int)}
+    '''
+    
     # define ifttt webhock url
     url = 'https://maker.ifttt.com/trigger/{event_name}/json/with/key/{key}'
 
