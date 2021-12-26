0a3480fa47f0d1df4b47f7f18e0ed8da99203583
floroe1988 <68111417+floroe1988@users.noreply.github.com>
2021-12-26 15:26:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     '''
     
     result = getConnectionSpeed()
-    storeConnectionSpeed(result)
+    response = storeConnectionSpeed(result)
 
 if __name__ == "__main__":
     while True:
